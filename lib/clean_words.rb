95269fcc53d89a186238c3c898530ead7d09fadc
Jake Sutton <jcs@foraker.com>
2014-10-23 16:35:00 -0600

    Fetch random words

--- lib/clean_words.rb
+++ lib/clean_words.rb
@@ -1,5 +1,48 @@
 require "clean_words/version"
 
 module CleanWords
-  #TODO
+  class Random
+    def self.fetch(size=nil, count=1)
+      new(size, count).fetch
+    end
+
+    def initialize(size=nil, count=1)
+      @size = size
+      @count = count
+    end
+
+    def fetch
+      @count > 1 ? @count.times.map { get_word } : get_word
+    end
+
+    private
+
+    def get_word
+      word_pool.shuffle.pop
+    end
+
+    def word_pool
+      @size.nil? ? words : words_for_size
+    end
+
+    def words
+      @words ||= File.new(file_path).readlines.map(&:chomp)
+    end
+
+    def file_path
+      File.dirname(__FILE__) + '/../data/words.txt'
+    end
+
+    def words_for_size
+      sizes.map { |size| words_by_length[size] }.flatten
+    end
+
+    def sizes
+      @size.respond_to?(:to_a) ? @size.to_a : [@size]
+    end
+
+    def words_by_length
+      @words_by_length ||= words.group_by(&:size)
+    end
+  end
 end
---
+++ spec/clean_words_spec.rb
@@ -0,0 +1,49 @@
+require File.expand_path("spec_helper", File.dirname(__FILE__))
+
+module CleanWords
+  describe Random do
+    describe '#fetch' do
+      it 'calls fetch on a new instance' do
+        words = double(fetch: 'test')
+        allow(CleanWords::Random).to receive(:new).and_return(words)
+
+        expect(CleanWords::Random.fetch).to eq 'test'
+      end
+    end
+
+    describe '.fetch' do
+      it 'returns a word' do
+        words = CleanWords::Random.new
+        allow(words).to receive(:words).and_return(%w(test))
+
+        expect(words.fetch).to eq 'test'
+      end
+
+      it 'returns the specified number of words' do
+        words = CleanWords::Random.new(nil, 3).fetch
+
+        expect(words.size).to eq 3
+      end
+
+      it 'returns a word of the specified size' do
+        word = CleanWords::Random.new(5).fetch
+
+        expect(word.size).to eq 5
+      end
+
+      it 'returns words that match an array of size options' do
+        words = CleanWords::Random.new([5,7], 100).fetch
+        sizes = words.map(&:size).uniq
+
+        expect(sizes).to match_array [5,7]
+      end
+
+      it 'returns words that match a range of sizes' do
+        words = CleanWords::Random.new((4..6), 100).fetch
+        sizes = words.map(&:size).uniq
+
+        expect(sizes).to match_array [4,5,6]
+      end
+    end
+  end
+end
